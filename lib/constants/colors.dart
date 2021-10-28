b6829767300322db662a04323db31772450c00aa
181069 <181069@ppu.edu.ps>
2021-10-28 09:56:32 +0300

    d

--- lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-class MyColors {
-  MyColors._();
-
-  static const String Cream = 'assets/images/cream.jpeg';
-  static const String ShampooImage = 'assets/images/shampooImage.png';
-  static  const String orangeImage='./assets/images/orange.jpeg';
-  static  const String lemmonImage='';
-
-}
